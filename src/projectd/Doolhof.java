d0dbce42ff2b995345d1889393a343fa56f4b71c
Rva03 <raalst@hotmail.nl>
2015-06-10 19:57:06 +0200

    Welkomstscherm toegevoegd Level 2 + Level 3 toegevoegd Herstarten van level is nu mogelijk

--- src/projectd/Doolhof.java
+++ src/projectd/Doolhof.java
@@ -21,21 +21,61 @@ public class Doolhof {
     /**
      * @param args the command line arguments
      */
-    private static JFrame frame;
-    private static JFrame gameFrame;
-    private static JPanel panel;
-    private static JPanel gamePanel;
-    private static JButton startButton;
-    private static JButton level1Button;
+    
     private static Level level1;
     private static Level level2;
     private static Level level3;
+    private static JFrame startscherm;
+    private static Scherm frame1;
+    private static Scherm frame2;
+    private static Scherm frame3;
     
     public static void main(String[] args) {
 
-        maakLevel1();
-        startLevel1();
+        maakLevels();
+        startscherm = new JFrame();
+        startscherm.setSize(750, 500);
+        startscherm.setTitle("Doolhof Spel");
+        startscherm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton start1Button = new JButton( new AbstractAction("Start level 1") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            startscherm.dispose();
+            startLevel1();
+        }
+        });
+        JButton start2Button = new JButton( new AbstractAction("Start level 2") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            startscherm.dispose();
+            startLevel2();
+        }
+        });
+        JButton start3Button = new JButton( new AbstractAction("Start level 3") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            startscherm.dispose();
+            startLevel3();
+        }
+        });
+        JPanel startpanel = new JPanel();
+        startpanel.setLayout(new BorderLayout());
+        JLabel titel = new JLabel("Het enige echte Doolhof-Doolhof spel!");
+        JLabel creators = new JLabel("Gemaakt door: Robin van Aalst");
+        startpanel.add(titel, BorderLayout.NORTH);
+        startpanel.add(start1Button, BorderLayout.WEST);
+        startpanel.add(start2Button, BorderLayout.CENTER);
+        startpanel.add(start3Button, BorderLayout.EAST);
+        startpanel.add(creators, BorderLayout.SOUTH);
+        startscherm.add(startpanel);
+        startscherm.setVisible(true);
         
+    }
+    
+    public static void maakLevels(){
+        maakLevel1();
+        maakLevel2();
+        maakLevel3();
     }
     
     public static void maakLevel1(){
@@ -52,7 +92,40 @@ public class Doolhof {
             {2,2,2,2,2,2,2,2,2,2,2,2}
         };
         level1 = new Level(maze1, 12, 1);
-        ArrayList<Veld> velden = level1.getVelden();
+        
+    }
+    
+    public static void maakLevel2(){
+        int[][] maze1 = { 
+            {2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,0,1,0,1,0,1,0,0,0,0,2},
+            {2,0,1,0,0,0,1,0,1,1,0,2},
+            {2,0,0,0,1,1,1,0,0,0,0,2},
+            {2,0,1,0,0,0,0,0,0,1,1,2},
+            {2,0,1,0,1,1,1,0,1,1,0,2},
+            {2,0,1,0,1,0,0,0,0,0,0,2},
+            {2,0,1,0,1,1,1,0,1,1,0,2},
+            {2,0,0,0,0,0,0,0,0,1,0,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2}
+        };
+        level2 = new Level(maze1, 12, 2);
+        
+    }
+    
+    public static void maakLevel3(){
+        int[][] maze1 = { 
+            {2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,0,0,0,1,0,1,0,0,0,0,2},
+            {2,1,1,0,0,0,1,0,1,1,0,2},
+            {2,0,0,0,1,1,1,0,0,0,0,2},
+            {2,0,1,0,0,0,0,0,1,1,0,2},
+            {2,0,1,0,1,1,1,0,1,0,0,2},
+            {2,0,1,1,1,0,0,0,0,1,0,2},
+            {2,0,1,0,1,1,1,0,1,1,0,2},
+            {2,0,0,0,0,0,0,0,0,1,0,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2}
+        };
+        level3 = new Level(maze1, 12, 3);
         
     }
 
@@ -60,35 +133,72 @@ public class Doolhof {
         Karakter karakter = new Karakter(40, 40, level1);
         VeldComponent component = new VeldComponent(level1.getVelden(), level1.getGrootte(), karakter);
         
-        Scherm frame = new Scherm(karakter, component, level1);
-        frame.setSize(750, 500);
-        frame.setTitle("Doolhof Spel");
-        frame.setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1 = new Scherm(karakter, component, level1);
+        frame1.setSize(750, 500);
+        frame1.setTitle("Doolhof Spel");
+        frame1.setLayout(new BorderLayout());
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        frame1.add(component);
         
-//        JPanel panel = new JPanel();
-//        panel.setBackground(Color.red);
-//        Dimension panelSize = new Dimension(800, 40);
-//        panel.setPreferredSize(panelSize);
+        JButton startButton = new JButton( new AbstractAction("Start level 1 opnieuw") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            frame1.dispose();
+            startLevel1();
+        }
+        });
+        frame1.add(startButton, BorderLayout.NORTH);
         
-//        JPanel panel2 = new JPanel();
-//        panel2.setBackground(Color.yellow);
-//        Dimension panel2Size = new Dimension(800, 100);
-//        panel2.setPreferredSize(panel2Size); 
+        frame1.setVisible(true);
+    }
+    
+    private static void startLevel2() {
+        Karakter karakter = new Karakter(40, 40, level2);
+        VeldComponent component = new VeldComponent(level2.getVelden(), level2.getGrootte(), karakter);
         
-        frame.add(component);
+        frame2 = new Scherm(karakter, component, level2);
+        frame2.setSize(750, 500);
+        frame2.setTitle("Doolhof Spel");
+        frame2.setLayout(new BorderLayout());
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        JButton startButton = new JButton("Level 1");
-        //Dimension startButtonSize = new Dimension(100, 25);
-        //startButton.setPreferredSize(startButtonSize);
-        frame.add(startButton, BorderLayout.NORTH);
-        ClickListener listener = new ClickListener(component, frame);
-        startButton.addActionListener(listener);
+        frame2.add(component);
         
-        //frame.add(panel, BorderLayout.SOUTH);
-        //frame.add(panel2, BorderLayout.CENTER);
-        frame.setVisible(true);
+        JButton startButton = new JButton( new AbstractAction("Start level 2 opnieuw") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            frame2.dispose();
+            startLevel2();
+        }
+        });
+        frame2.add(startButton, BorderLayout.NORTH);
+        
+        frame2.setVisible(true);
+    }
+    
+    private static void startLevel3() {
+        Karakter karakter = new Karakter(40, 40, level3);
+        VeldComponent component = new VeldComponent(level3.getVelden(), level3.getGrootte(), karakter);
+        
+        frame3 = new Scherm(karakter, component, level3);
+        frame3.setSize(750, 500);
+        frame3.setTitle("Doolhof Spel");
+        frame3.setLayout(new BorderLayout());
+        frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame3.add(component);
+        
+        JButton startButton = new JButton( new AbstractAction("Start level 3 opnieuw") {
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+            frame3.dispose();
+            startLevel3();
+        }
+        });
+        frame3.add(startButton, BorderLayout.NORTH);
+        
+        frame3.setVisible(true);
     }
     
     
